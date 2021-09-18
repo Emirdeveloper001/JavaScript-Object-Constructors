41b797d492b1e7caaa89256a0b5645423c880ef1
unknown <emirdeveloper2001@gmail.com>
2021-09-18 16:31:52 +0430

    First constructor made!

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var me = { name: "Emir", age: "20", job: "programmer" };
 // console.log(me);
 console.log(me.name);
+
+function Person(name, family, age, job) {
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    this.job = job;
+}
+var Michael = new Person("Michael", "scofield", "33", "Engineer");
+console.log(Michael);
